f6a5b53ca27c7627f6190e9bf7738841661aae2a
Yeganeh Marghi <ymmarghi@gmail.com>
2024-02-13 13:41:04 -0800

    Update - descriptions of the classes

--- mmidas/eval_models.py
+++ mmidas/eval_models.py
@@ -1,20 +1,13 @@
-import os
-from utils.training import train_cplmixVAE
-from utils.training_VAE import train_VAE
+import numpy as np
 import pickle
-from utils.state_analysis import state_analyzer
-from scipy.optimize import linear_sum_assignment
+import seaborn as sns
 import matplotlib.pyplot as plt
-import random
-from utils.dataloader import load_data
-from utils.config import load_config
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from scipy.special import softmax
-import seaborn as sns
-import glob
-import numpy as np
-from utils.data_tools import reorder_genes
-
+from .data_tools import reorder_genes
+from .cpl_mixvae import cpl_mixVAE
+from .dataloader import load_data
+from .config import load_config
 
 
 def eval_mixmodel(data,
@@ -22,17 +15,13 @@ def eval_mixmodel(data,
                    n_arm,
                    state_dim,
                    latent_dim=30,
-                   latent_dim_ae=2,
                    fc_dim=100,
                    temp=1,
                    saving_folder='',
-                   ae_folder='',
-                   ae_model='',
                    device=None,
                    hard=False,
                    tau=0.01,
                    n_gene=0,
-                   n_zim=1,
                    batch_size=5000,
                    models=[],
                    loss_mode='MSE',
@@ -41,22 +30,44 @@ def eval_mixmodel(data,
                    variational=True,
                    ref_pc=False,
                    save=False):
+    
+    """
+        Initialized the deep mixture model and its optimizer.
+
+        input args:
+            data: a dictionary that contains the following, 'log1p' for the gene expression matrix, 
+                  'cluster_id' for the cluster label, and 'cluster_label' for the cluster name, 'gene_id' for the gene names.
+            n_categories: number of categories in the categorical variable.
+            n_arm: int value that indicates number of arms.
+            state_dim: dimension of the state variable.
+            latent_dim: dimension of the latent representation.
+            fc_dim: dimension of the hidden layer.
+            temp: temperature of sampling.
+            saving_folder: a string that indicates the folder to save the model(s) and file(s).
+            device: device: computing device, either 'cpu' or 'cuda'. For 'cpu' mode, the device is None. For 'cuda' mode, the device is an integer.
+            hard: a boolean variable, True uses one-hot method that is used in Gumbel-softmax, and False uses the Gumbel-softmax function.
+            tau: temperature of the Gumbel-softmax function.
+            n_gene: number of genes to be used in the analysis.
+            batch_size: batch size for dataloader.
+            models: a list of pre-trained models.
+            loss_mode: the loss function, either 'MSE' or 'ZINB'.
+            subclass: a string that indicates the subclass of the data, e.g. 'Glutamatergic', 'GABAergic', or 'Sst'.
+            sort_gene: a boolean variable, True sorts the genes based on the variance, False does not sort the genes.
+            variational: a boolean variable for variational mode, False mode does not use sampling.
+            ref_pc: a boolean variable, True uses the reference prior for the categorical variable.
+            save: a boolean variable, True saves the model and the results, False does not save the model and the results.
+    """
 
 
     paths = load_config(config_file='config.toml')
     if len(data) == 0:
         if subclass:
             data_file = paths['local_data_path'] / paths['data_' + subclass]
-            data = load_data(datafile=data_file)  # , ref_types=True, ann_smt=paths['ann_smt'], ann_10x=paths['ann_10x'])
+            data = load_data(datafile=data_file)  
 
             if sort_gene:
                 g_index = reorder_genes(data['log1p'])
-                # if n_gene == 0:
-                #     n_gene = len(g_index) // n_arm
-
-                # g_idx = g_index[0::n_arm]
-                # for arm in range(1, n_arm):
-                #     g_idx = np.concatenate((g_idx, g_index[arm::n_arm]))
+            
 
             data['log1p'] = data['log1p'][:, g_index[:n_gene]]
             data['gene_id'] = data['gene_id'][g_index[:n_gene]]
@@ -88,19 +99,11 @@ def eval_mixmodel(data,
     if n_gene == 0:
         n_gene = data['log1p'].shape[1]
 
-    uniq_cluster_order = np.unique(data['cluster_order'])
-
-
-    cpl_mixVAE = train_cplmixVAE(saving_folder=saving_folder,
+    cplMixVAE = cpl_mixVAE(saving_folder=saving_folder,
                                  device=device,
                                  n_feature=n_gene)
 
-    train_loader, test_loader, alldata_loader, train_ind, test_ind = cpl_mixVAE.getdata(dataset=data['log1p'],
-                                                                                        label=data['cluster_order'],
-                                                                                        batch_size=batch_size)
-    data_indx = np.concatenate((train_ind, test_ind))
-
-    cpl_mixVAE.init_model(n_categories=n_categories,
+    cplMixVAE.init_model(n_categories=n_categories,
                           state_dim=state_dim,
                           input_dim=n_gene,
                           fc_dim=fc_dim,
@@ -119,16 +122,6 @@ def eval_mixmodel(data,
                           variational=variational,
                           ref_prior=ref_pc)
 
-    if ae_folder:
-        ae = train_VAE(saving_folder=ae_folder, device=device)
-
-        ae.init_model(input_dim=data['log1p'].shape[1],
-                      fc_dim=fc_dim,
-                      lowD_dim=latent_dim_ae,
-                      x_drop=0.,
-                      lr=0.,
-                      variational=False)
-
     recon_loss = []
     label_pred = []
     test_dist_z = []
@@ -145,26 +138,15 @@ def eval_mixmodel(data,
     z_ae = []
     data_rec = []
 
-    data['cluster_id'] = data['cluster_id'][data_indx]
 
     for i in range(len(models)):
         print(models[i])
-        cpl_mixVAE.load_model(models[i])
+        cplMixVAE.load_model(models[i])
         if ref_pc:
-            # data_file_id = cpl_mixVAE.eval_model(data_loader=alldata_loader, g_idx=g_idx, gc_p=data['c_p'], c_onehot=data['c_onehot'], batch_size=batch_size)
-            outcome, data_file_id = cpl_mixVAE.eval_model(data_loader=alldata_loader, g_idx=g_idx, gc_p=data['c_p'], c_onehot=data['c_onehot'], batch_size=batch_size)
+            outcome = cplMixVAE.eval_model(data_mat=data['log1p'], c_p=data['c_p'], batch_size=batch_size, mode=loss_mode)
         else:
-            # data_file_id = cpl_mixVAE.eval_model(data_loader=alldata_loader, g_idx=g_idx, batch_size=batch_size)
-            outcome, data_file_id = cpl_mixVAE.eval_model(data_mat=data['log1p'], batch_size=batch_size, mode=loss_mode)
+            outcome = cplMixVAE.eval_model(data_mat=data['log1p'], batch_size=batch_size, mode=loss_mode)
 
-        if ae_folder:
-            ae.load_model(ae_model)
-            ae_file = ae.eval_model(data_mat=data['log1p'])
-            ae_out = ae.load_file(ae_file)
-            z_ae = ae_out['z']
-            data_rec = ae_out['x_recon']
-
-        # outcome = cpl_mixVAE.load_file(data_file_id)
         x_low = outcome['x_low']
         predicted_label = outcome['predicted_label']
         test_dist_z.append(outcome['total_dist_z'])
@@ -180,7 +162,6 @@ def eval_mixmodel(data,
 
         for arm in range(n_arm):
             test_loss[arm].append(outcome['total_loss_rec'][arm])
-
             label_predict = []
             for d in range(len(data['cluster_id'])):
                 z_cat = np.squeeze(c_prob[arm][d, :])
@@ -211,8 +192,6 @@ def eval_mixmodel(data,
                 armA_vs_armB = armA_vs_armB[:, nprune_indx][nprune_indx]
                 diag_term = np.diag(armA_vs_armB_norm)
                 ind_sort = np.argsort(diag_term)
-                # armA_vs_armB_norm = armA_vs_armB_norm[:, ind_sort[::-1]][ind_sort[::-1]]
-                # armA_vs_armB = armA_vs_armB[:, ind_sort[::-1]][ind_sort[::-1]]
                 consensus_min.append(np.min(diag_term))
                 con_mean = 1. - (sum(np.abs(predicted_label[0, :] - predicted_label[1, :]) > 0.) / predicted_label.shape[1])
                 consensus_mean.append(con_mean)
@@ -252,7 +231,7 @@ def eval_mixmodel(data,
         pickle.dump(data_dic, f)
         f.close()
 
-    return cpl_mixVAE, data, data_dic
+    return cplMixVAE, data, data_dic
 
 
 
