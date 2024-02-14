7a9c886a55c1226eb69440b54c3ad0a0242f5bff
Yeganeh Marghi <ymmarghi@gmail.com>
2024-02-14 15:21:51 -0800

    Fix - issues with device setting

--- mmidas/utils/dataloader.py
+++ mmidas/utils/dataloader.py
@@ -39,6 +39,7 @@ def load_data(datafile, n_gene=0, gene_id=[], rmv_type=[], min_num=10, eps=1e-1,
             if key not in ['gene_id']:
                 data[key] = np.delete(data[key], ind, axis=0)
 
+    uniq_clusters = np.unique(data['cluster_label'])
     count = np.zeros(len(uniq_clusters))
     for it, tt in enumerate(uniq_clusters):
         count[it] = sum(data['cluster_label'] == tt)
@@ -50,7 +51,7 @@ def load_data(datafile, n_gene=0, gene_id=[], rmv_type=[], min_num=10, eps=1e-1,
 
     label_encoder = LabelEncoder()
     integer_encoded = label_encoder.fit_transform(data['cluster_id'])
-    onehot_encoder = OneHotEncoder(sparse=False)
+    onehot_encoder = OneHotEncoder(sparse_output=False)
     integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
     data['c_onehot'] = onehot_encoder.fit_transform(integer_encoded)
     data['c_p'] = softmax((data['c_onehot'] + eps) / tau, axis=1)
@@ -62,63 +63,50 @@ def load_data(datafile, n_gene=0, gene_id=[], rmv_type=[], min_num=10, eps=1e-1,
     return data
 
 
-def data_gen(dataset, train_size, index):
+def data_gen(dataset, train_size, seed):
 
         test_size = dataset.shape[0] - train_size
         train_cpm, test_cpm, train_ind, test_ind = train_test_split(
-            dataset[:, index], np.arange(dataset.shape[0]), train_size=train_size, test_size=test_size, random_state=0)
+            dataset, np.arange(dataset.shape[0]), train_size=train_size, test_size=test_size, random_state=seed)
 
-        train_cpm, val_cpm, train_ind, val_ind = train_test_split(train_cpm, train_ind, train_size=train_size - test_size, test_size=test_size, random_state=0)
-
-        return train_cpm, val_cpm, test_cpm, train_ind, val_ind, test_ind
+        return train_cpm, test_cpm, train_ind, test_ind
 
 
-def get_loaders(dataset, label=[], index=[], batch_size=128, train_size=0.9):
+def get_loaders(dataset, label=[], seed=None, batch_size=128, train_size=0.9):
 
         batch_size = batch_size
-
-        if len(index) == 0:
-            index = np.arange(0, dataset.shape[1])
 
         if len(label) > 0:
             train_ind, val_ind, test_ind = [], [], []
             for ll in np.unique(label):
                 indx = np.where(label == ll)[0]
                 tt_size = int(train_size * sum(label == ll))
-                _, _, _, train_subind, val_subind, test_subind = data_gen(dataset[indx, :], tt_size, index)
+                _, _, train_subind, test_subind = data_gen(dataset, tt_size, seed)
                 train_ind.append(indx[train_subind])
-                val_ind.append(indx[val_subind])
                 test_ind.append(indx[test_subind])
 
             train_ind = np.concatenate(train_ind)
-            val_ind = np.concatenate(val_ind)
             test_ind = np.concatenate(test_ind)
             train_set = dataset[train_ind, :]
-            val_set = dataset[val_ind, :]
             test_set = dataset[test_ind, :]
             n_class = len(np.unique(label))
         else:
             tt_size = int(train_size * dataset.shape[0])
-            train_set, val_set, test_set, train_ind, val_ind, test_ind = data_gen(dataset, tt_size, index)
+            train_set, test_set, train_ind, test_ind = data_gen(dataset, tt_size, seed)
 
         train_set_torch = torch.FloatTensor(train_set)
         train_ind_torch = torch.FloatTensor(train_ind)
         train_data = TensorDataset(train_set_torch, train_ind_torch)
         train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True, drop_last=True, pin_memory=True)
 
-        val_set_torch = torch.FloatTensor(val_set)
-        val_ind_torch = torch.FloatTensor(val_ind)
-        validation_data = TensorDataset(val_set_torch, val_ind_torch)
-        validation_loader = DataLoader(validation_data, batch_size=batch_size, shuffle=True, drop_last=False, pin_memory=True)
-
         test_set_torch = torch.FloatTensor(test_set)
         test_ind_torch = torch.FloatTensor(test_ind)
         test_data = TensorDataset(test_set_torch, test_ind_torch)
         test_loader = DataLoader(test_data, batch_size=1, shuffle=True, drop_last=False, pin_memory=True)
 
-        data_set_troch = torch.FloatTensor(dataset[:, index])
+        data_set_troch = torch.FloatTensor(dataset)
         all_ind_torch = torch.FloatTensor(range(dataset.shape[0]))
         all_data = TensorDataset(data_set_troch, all_ind_torch)
         alldata_loader = DataLoader(all_data, batch_size=batch_size, shuffle=False, drop_last=False, pin_memory=True)
 
-        return train_loader, validation_loader, test_loader, alldata_loader
+        return train_loader, test_loader, alldata_loader
